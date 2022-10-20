879df1f7de586ecd3fc727a03fcc70b616565f7e
Mohamud <msheeq99@outlook.com>
2022-10-20 14:02:23 +0100

    on the commit i displayed my quiz box in the center of my contain also including the timer box, in the middle of my container i have the quiz questions

---
+++ script.js
@@ -0,0 +1,16 @@
+// if start quiz button clicked 
+start_bnt.onclick = ()=> {
+    info-box.classList.add("activeInfo"); //show the info box
+}
+
+// if exit button clicked 
+exit_bnt.onclick = ()=> {
+    info-box.classList.add("activeInfo"); //hide the info box
+}
+
+// if continue button clicked 
+continue_bnt.onclick = ()=> {
+    info-box.classList.add("activeInfo"); //show the info box
+    quiz-box.classList.add("activeInfo"); //show the info box
+}
+
