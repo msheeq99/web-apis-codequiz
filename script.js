e57c94ab45ab1cded681f71885c7807e01273f49
Mohamud <msheeq99@outlook.com>
2022-10-20 17:07:13 +0100

    on script.js quizData added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const startQuiz = document.querySelector("#startBtn");
 const infoBox = document.querySelector(".info-box");
 const exitBtn = document.querySelector("#quit");
 const continueBtn = document.querySelector("#restart");
-const quizBox = document.querySelector("#quiz-")
+const quizBox = document.querySelector("#quiz-box");
 
 startQuiz.addEventListener('click', () =>  {
     startQuiz.style.display = "none";
@@ -16,9 +16,101 @@ exitBtn.addEventListener('click', () => {
 
 continueBtn.addEventListener('click', () => {
     infoBox.style.display = "none";
+    quizBox.style.display = "block";
     
 })
 
-function setNextQuestion() {
+function setNextQuestion() [
+   
 
-}
+const quizData = {
+    {
+        question: "inside which HTML element do we put the JavaScript?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+        correct: ,
+    },
+
+    {
+        question: "inside which HTML element do we put the JavaScript?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+        correct: ,
+    },
+
+    {
+        question: "inside which HTML element do we put the JavaScript?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+        correct: ,
+    },
+
+    {
+        question: "inside which HTML element do we put the JavaScript?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+        correct: ,
+    },
+
+    {
+        question: "inside which HTML element do we put the JavaScript?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+        correct: ,
+    },
+
+    {
+        question: "inside which HTML element do we put the JavaScript?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+        correct: ,
+    },
+
+    {
+        question: "inside which HTML element do we put the JavaScript?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+        correct: ,
+    },
+
+    {
+        question: "inside which HTML element do we put the JavaScript?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+        correct: ,
+    },
+
+    {
+        question: "inside which HTML element do we put the JavaScript?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+        correct: ,
+    },
+
+    {
+        question: "inside which HTML element do we put the JavaScript?",
+        a: ,
+        b: ,
+        c: ,
+        d: ,
+        correct: ,
+    },
+]
