cfd0c73d7da7cda4b44254eb3fc6086c68e1b99a
Mohamud <msheeq99@outlook.com>
2022-10-20 16:47:51 +0100

    once clicked my start and info show i made my exit button take me back to the start of the start, next commit i will be working on the continue button

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 const startQuiz = document.querySelector("#startBtn");
-const infoBox = document.querySelector('.info-box');
+const infoBox = document.querySelector(".info-box");
+const exitBtn = document.querySelector("#quit");
+const continueBtn = document.querySelector("#restart");
+const quizBox = document.querySelector("#quiz-")
 
 startQuiz.addEventListener('click', () =>  {
     startQuiz.style.display = "none";
     infoBox.style.display = "block";
 })
 
+exitBtn.addEventListener('click', () => {
+    infoBox.style.display = "none";
+    startQuiz.style.display = "block";
+})
+
+continueBtn.addEventListener('click', () => {
+    infoBox.style.display = "none";
+    
+})
 
 function setNextQuestion() {
 
