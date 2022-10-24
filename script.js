1cd9fd9928e5f2b65a83d635b47c678b7fa63049
Mohamud <msheeq99@outlook.com>
2022-10-24 21:44:37 +0100

    defined the timer function so the timer actives as soon as the user starts the quiz

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ const continueBtn = document.querySelector("#restart");
 const resultsBox = document.querySelector("#results-box")
 const quizBox = document.querySelector("#quiz-box");
 const replayBtn = document.querySelector("#replay");
-
+const options_list = document.querySelector(".option-list");
 const quitBtn = document.querySelector("#exit-btn");
+const timeCount = quizBox.querySelector(".timer .timer-sec");
 //start button clicked
 startQuiz.addEventListener('click', () =>  {
     startQuiz.style.display = "none";
@@ -26,11 +27,13 @@ continueBtn.addEventListener('click', () => {
     quizBox.style.display = "block";
     showQuestions(0);
     queCounter(1);
-    
+    startTimer(15);
 })
 
 let que_count = 0;
 let que_numb = 1;
+let counter;
+let timeValue = 15;
 
 const next_btn = document.querySelector("#next-btn");
 
@@ -41,6 +44,8 @@ next_btn.onclick = ()=> {
         que_numb++;
         showQuestions(que_count);
         queCounter(que_numb);
+        clearInterval(counter);
+        startTimer(timeValue);
     }else {
         console.log("Questions completed");
     }
@@ -51,12 +56,12 @@ next_btn.onclick = ()=> {
 //Getting questions and options from array
 function showQuestions(index){
     const que_text = document.querySelector(".que-text");
-    const options_list = document.querySelector(".option-list");
-    let que_tag = '<span>'+questions[index].numb + questions[index].question + '</span>';
+    let que_tag = '<span>'+questions[index].numb+ '.' + questions[index].question + '</span>';
     let option_tag = '<div class= "option">'+ questions[index].options[0] +'<span></span></div>'
                      + '<div class= "option">'+ questions[index].options[1] +'<span></span></div>'
                      + '<div class= "option">'+ questions[index].options[2] +'<span></span></div>'
                      + '<div class= "option">'+ questions[index].options[3] +'<span></span></div>';
+    que_text.innerHTML = que_tag
     options_list.innerHTML = option_tag;
     const option = options_list.querySelectorAll(".option");
     for (let i = 0; i < option.length; i++) {
@@ -64,22 +69,47 @@ function showQuestions(index){
     }
 }
 
+let tick = '<div class="tick"><i class="fas fa-check"></i></div>';
+let cross = '<div class="cross"><i class="fas fa-times"></i></div>';
+
 function optionSelected(answer){
+    clearInterval(counter);
     let userAns = answer.textContent;
     let correctAns = questions[que_count].answer;
+    let allOptions = options_list.children.length;
     if(userAns == correctAns){
-        answer.classList.add("correct")
+        answer.classList.add("correct");
         console.log ("Answer is Correct");
+        answer.insertAdjacentHTML("beforeend", tick);
     }else{
-        answer.classList.add("incorrect")
-        console.log("Answer is Wrong"); 
+        answer.classList.add("incorrect");
+        console.log("Answer is Wrong");
+        answer.insertAdjacentHTML("beforeend", cross);
+        //if answer is incorrect then automatically selected the correct answer
+        for (let i = 0; i < allOptions; i++) {
+            if(options_list.children[i].textContent == correctAns) {
+                options_list.children[i].setAttribute("class", "option correct");
+            }
+        }
+    }
+
+    //once user selected disabled all options
+    for (let i = 0; i < allOptions; i++){
+        options_list.children[i].classList.add("disabled");
+    }
+}
+
+function startTimer(time){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time;
+        time--;
     }
 }
 
 
 
-
-
+ 
 
 function queCounter(index){
     const bottom_ques_counter = quizBox.querySelector(".total-que");
